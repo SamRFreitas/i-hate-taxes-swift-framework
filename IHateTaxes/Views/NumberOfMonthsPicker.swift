f91b382ec277be3446168b0f753ae5aded9cdf37
SamuelDac <samuel@dacxi.com>
2025-01-08 22:33:02 -0300

    Implement Picker

--- IHateTaxes/Views/NumberOfMonthsPicker.swift
+++ IHateTaxes/Views/NumberOfMonthsPicker.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct NumberOfMonthsPicker: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    @Binding var selectedMonth: Int
+       
+       var body: some View {
+           VStack {
+               Text("Escolha o número de meses")
+               
+               Picker("Meses", selection: $selectedMonth) {
+                   ForEach(1..<13) { month in
+                       Text("\(month) \(month == 1 ? "mês" : "meses" )")
+                           .tag(month)
+                   }
+               }
+               .onChange(of: selectedMonth) { _, newValue in
+                   // Atualizar o valor quando o mês mudar
+                   selectedMonth = newValue
+               }
+               #if os(macOS)
+                   .pickerStyle(MenuPickerStyle()) // Usado para macOS
+               #else
+                   .pickerStyle(WheelPickerStyle()) // Usado para iOS
+               #endif
+                   .clipped() // Garante que o picker não ultrapasse o limite de sua área
+               }
+               .padding()
+       }
 }
 
-#Preview {
-    NumberOfMonthsPicker()
+struct NumberOfMonthsPicker_Previews: PreviewProvider {
+    @State static var selectedMonth = 12
+
+    static var previews: some View {
+        NumberOfMonthsPicker(selectedMonth: $selectedMonth)
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .frame(width: 300, height: 300)
+    }
 }

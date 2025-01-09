e40c9a3c1e6e07796bd9f68ddecf2032a417a254
SamuelDac <samuel@dacxi.com>
2025-01-08 22:32:37 -0300

    Implement Expense Settings Form

--- IHateTaxes/Views/FormExpenseSettings.swift
+++ IHateTaxes/Views/FormExpenseSettings.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct FormExpenseSettings: View {
+    
+    @State private var minimumWage: Double = 0.0
+    @State private var proLaborePercentage: Double = 0.0
+    @State private var contabilizeiMonthlyFee: Double = 0.0
+    @State private var rental: Double = 0.0
+    @State private var digitalSignature: Double = 0.0
+    @State private var numberOfMonthsToSaveMoney: Int = 12
+    @State private var isNational: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            
+            VStack {
+                
+                AmountInput(title: "Salário Mínimo", amount: $minimumWage)
+                AmountInput(title: "Porcentagem Pró-Labore", amount: $proLaborePercentage)
+                AmountInput(title: "Mensalidade Contabilizei", amount: $contabilizeiMonthlyFee)
+                AmountInput(title: "Aluguel", amount: $rental)
+                AmountInput(title: "Assinatura Digital", amount: $digitalSignature)
+                NumberOfMonthsPicker(selectedMonth: $numberOfMonthsToSaveMoney)
+                Switch(isOn: $isNational)
+                
+            }
+            .padding()
+            
+        }
+        
     }
+    
+    
 }
 
-#Preview {
-    FormExpenseSettings()
+struct FormExpenseSettings_Previews: PreviewProvider {
+    
+    
+    static var previews: some View {
+        FormExpenseSettings()
+            .frame(width: 500, height: 600)
+    }
+    
+    
 }
+
+
